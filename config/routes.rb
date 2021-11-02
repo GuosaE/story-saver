6f620e3fdfa8d5b9e43e08f9fe9d7b42f1cabcb9
Guosa_Edebiri <gedebiri@gmail.com>
2021-11-02 05:43:34 -0500

    added article views

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   resources :subjects
   resources :categories
   resources :notes
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
