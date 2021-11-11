942e859bf7d159cd880d787103bca09b41c9ac53
Guosa_Edebiri <gedebiri@gmail.com>
2021-11-11 15:10:02 -0600

    minor changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
     def index
-        @countries = Country.all
+        # @countries = Country.all
         @articles = Article.order(:title)
     end
 end
